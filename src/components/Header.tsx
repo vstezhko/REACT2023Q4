627d16c0715624650f290444805984041831a824
viktoriastezko <viktoriastezhko@gmail.com>
2023-10-29 18:33:31 +0300

    refactor: change image path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,11 +11,7 @@ class Header extends Component<HeaderProps> {
     return (
       <header className="header">
         <div className="logo">
-          <img
-            className="logo__img"
-            src="/starwars.svg"
-            alt="logo"
-          />
+          <img className="logo__img" src="/starwars.svg" alt="logo" />
           <h2>STARWARS</h2>
         </div>
         <SearchBlock onSearch={this.props.onSearch} />
