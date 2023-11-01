7d7b27ede36f0b0e3dc9b28e6ca57f233dcb578c
viktoriastezko <viktoriastezhko@gmail.com>
2023-11-01 14:32:52 +0300

    refactor: change Header component to functional

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 import SearchBlock from './SearchBlock';
 import ErrorBtn from './ErrorBtn';
 
-interface HeaderProps {
+interface HeaderParams {
   onSearch: (newValue: string) => void;
 }
 
-class Header extends Component<HeaderProps> {
-  render() {
-    return (
-      <header className="header">
-        <div className="logo">
-          <img className="logo__img" src="/starwars.svg" alt="logo" />
-          <h2>STARWARS</h2>
-        </div>
-        <SearchBlock onSearch={this.props.onSearch} />
-        <ErrorBtn />
-      </header>
-    );
-  }
-}
+const Header: FC<HeaderParams> = ({ onSearch }) => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img className="logo__img" src="/starwars.svg" alt="logo" />
+        <h2>STARWARS</h2>
+      </div>
+      <SearchBlock onSearch={onSearch} />
+      <ErrorBtn />
+    </header>
+  );
+};
 
 export default Header;
