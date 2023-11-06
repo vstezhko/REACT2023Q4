ecb831df9b00592e002e5f3df0ed510b6fd77018
viktoriastezko <viktoriastezhko@gmail.com>
2023-11-06 10:49:44 +0300

    fix: fix app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Main from './pages/Main';
 import ErrorBoundary from './components/ErrorBoundary';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
@@ -17,18 +17,16 @@ const router = createBrowserRouter([
   },
 ]);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="background">
-        <ErrorBoundary
-          fallback={<p className="fallback">Something went wrong</p>}
-        >
-          <RouterProvider router={router} />
-        </ErrorBoundary>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="background">
+      <ErrorBoundary
+        fallback={<p className="fallback">Something went wrong</p>}
+      >
+        <RouterProvider router={router} />
+      </ErrorBoundary>
+    </div>
+  );
+};
 
 export default App;
