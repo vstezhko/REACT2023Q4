e7bad1781c6d093d19aefe1b1340005cbf1d5f2b
viktoriastezko <viktoriastezhko@gmail.com>
2023-11-09 14:32:44 +0300

    refactor: refactor styles to scss. refactor pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { BaseSyntheticEvent, FC, useMemo } from 'react';
 
 interface PaginationParams {
   handlePageChange: (newPage: number) => void;
@@ -11,70 +11,93 @@ const Pagination: FC<PaginationParams> = ({
   currentPage,
   pageCount = currentPage,
 }) => {
-  const onChange = (page: number) => {
-    if (page > 0 && page <= pageCount && page !== currentPage)
-      handlePageChange(page);
+  const startPage = 1;
+  const prevPage = currentPage - 1;
+  const nextPage = currentPage + 1;
+  const lastPage = pageCount;
+  const onChange = (e: BaseSyntheticEvent) => {
+    const targetPage: number = e.currentTarget.dataset.page;
+    const isTargetPageValid =
+      targetPage > 0 && targetPage <= pageCount && targetPage !== currentPage;
+
+    if (isTargetPageValid) handlePageChange(targetPage);
   };
+
+  const checkPage = (page: number) => {
+    if (currentPage === page) return 'pagination__item_inactive';
+  };
+
+  const checkStartPage = useMemo(() => {
+    return checkPage(startPage);
+  }, [currentPage]);
+
+  const checkLastPage = useMemo(() => {
+    return checkPage(pageCount);
+  }, [currentPage, pageCount]);
 
   return (
     <div className="pagination">
       <div
-        className={`pagination__item ${currentPage === 1 ? 'inactive' : ''}`}
-        onClick={() => onChange(1)}
+        className={`pagination__item ${checkStartPage}`}
+        data-page={startPage}
+        onClick={onChange}
       >
-        <img
-          className="pagination__img start"
-          src="/pag-2.svg"
-          alt="previous"
-        />
+        <img className="pagination__img start" src="/pag-2.svg" alt="start" />
       </div>
       <div
-        className={`pagination__item ${currentPage === 1 ? 'inactive' : ''}`}
-        onClick={() => onChange(currentPage - 1)}
+        className={`pagination__item ${checkStartPage}`}
+        data-page={prevPage}
+        onClick={onChange}
       >
         <img className="pagination__img prev" src="/pag-1.svg" alt="previous" />
       </div>
+
       {currentPage !== 1 ? (
         <div
           className="pagination__item"
-          onClick={() => onChange(currentPage - 1)}
+          data-page={prevPage}
+          onClick={onChange}
         >
-          <p>{currentPage - 1}</p>
+          <p>{prevPage}</p>
         </div>
-      ) : (
-        ''
-      )}
+      ) : null}
 
-      <div className="pagination__item active">
+      <div className="pagination__item pagination__item_active">
         <p>{currentPage}</p>
       </div>
 
       {pageCount > 2 && currentPage < pageCount ? (
         <div
           className="pagination__item"
-          onClick={() => onChange(currentPage + 1)}
+          data-page={nextPage}
+          onClick={onChange}
         >
-          <p>{currentPage + 1}</p>
+          <p>{nextPage}</p>
         </div>
-      ) : (
-        ''
-      )}
+      ) : null}
+
       <div
-        className={`pagination__item ${
-          currentPage === pageCount ? 'inactive' : ''
-        }`}
-        onClick={() => onChange(currentPage + 1)}
+        className={`pagination__item ${checkLastPage}`}
+        data-page={nextPage}
+        onClick={onChange}
       >
-        <img className="pagination__img next" src="/pag-1.svg" alt="previous" />
+        <img
+          className="pagination__img pagination__img_next"
+          src="/pag-1.svg"
+          alt="next"
+        />
       </div>
 
       <div
-        className={`pagination__item ${
-          currentPage === pageCount ? 'inactive' : ''
-        }`}
-        onClick={() => onChange(pageCount)}
+        className={`pagination__item ${checkLastPage}`}
+        data-page={lastPage}
+        onClick={onChange}
       >
-        <img className="pagination__img end" src="/pag-2.svg" alt="previous" />
+        <img
+          className="pagination__img pagination__img_last"
+          src="/pag-2.svg"
+          alt="last"
+        />
       </div>
     </div>
   );
