b88f183c8aacb5ac47feb28f9ec32db5027b405c
viktoriastezko <viktoriastezhko@gmail.com>
2023-11-13 00:02:32 +0300

    feat: add details test

--- test/details.test.tsx
+++ test/details.test.tsx
@@ -1,11 +1,16 @@
 import { describe, it, expect, vi } from 'vitest';
-import { render } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Details from '../src/components/Details';
+import { characterMock } from './mock/characterMock';
 
 vi.mock('../api/Api.Service', () => {
   return {
-    getCharacter: vi.fn(),
+    getCharacter: () => {
+      setTimeout(() => {
+        return characterMock;
+      }, 1000);
+    },
   };
 });
 
@@ -15,20 +20,33 @@ vi.mock('react-router-dom', async () => {
     ...actual,
     useParams: () => {
       return {
-        id: 'idString',
+        id: id,
       };
     },
   };
 });
 
+const id = '309c1379-8579-4401-916e-bc6a17b2b39f';
+
 describe('Details Component', () => {
   it('displays loading indicator while fetching data', async () => {
     const { getByText } = render(
-      <MemoryRouter initialEntries={['/details/idString']}>
+      <MemoryRouter initialEntries={[`/details/${id}`]}>
         <Details />
       </MemoryRouter>
     );
     const loading = await getByText('loading...');
     expect(loading).toBeInTheDocument();
+    cleanup();
+  }, 500);
+
+  it('Make sure the detailed card component correctly displays the detailed card data', async () => {
+    const { findByText } = render(
+      <MemoryRouter initialEntries={[`/details/${id}`]}>
+        <Details />
+      </MemoryRouter>
+    );
+    const nameLabel = await findByText(characterMock.data.attributes.name);
+    expect(nameLabel).toBeInTheDocument();
   });
 });
