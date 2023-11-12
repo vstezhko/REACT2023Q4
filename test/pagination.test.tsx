6488ba9b376973af2ecd75a18b2f560bf9ed7816
viktoriastezko <viktoriastezhko@gmail.com>
2023-11-13 00:06:39 +0300

    feat: remove unused code

--- test/pagination.test.tsx
+++ test/pagination.test.tsx
@@ -3,7 +3,6 @@ import { test, expect, describe } from 'vitest';
 import DataProvider from '../src/components/DataProvider';
 import Main from '../src/pages/Main';
 import { MemoryRouter } from 'react-router-dom';
-import { userEvent } from '@testing-library/user-event';
 
 describe('pagination tests', () => {
   test('Pagination exists after getting data from server', async () => {
@@ -16,31 +15,6 @@ describe('pagination tests', () => {
     );
     const pagination = await screen.findByTestId('pagination');
     expect(pagination).toBeInTheDocument();
-    cleanup();
-  });
-  test('Make sure the component updates URL query parameter when page changes', async () => {
-    render(
-      <MemoryRouter>
-        <DataProvider>
-          <Main />
-        </DataProvider>
-      </MemoryRouter>
-    );
-    const paginationBtns = await screen.findAllByTestId('paginationBtn');
-    const nonDisabledPaginationBtns = paginationBtns.filter(
-      (button) => button.className === 'pagination__item'
-    );
-    // let urlBeforeClick = document.URL;
-    nonDisabledPaginationBtns.forEach((btn) => {
-      userEvent.click(btn);
-      // console.log(btn);
-      // const urlAfterClick = window.history;
-      // console.log(urlAfterClick);
-    });
-
-    // userEvent.click(nonDisabledPaginationBtns.)
-
-    // expect(pagination).toBeInTheDocument();
     cleanup();
   });
 });
