58a68d79928e93ea120eb99fe1acc4f9a9d51d63
viktoriastezko <viktoriastezhko@gmail.com>
2023-11-19 22:11:14 +0300

    feat: add query tests

--- test/api.test.tsx
+++ test/api.test.tsx
@@ -1,16 +1,47 @@
-import { describe, it } from 'vitest';
-import { renderHook } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import { renderHook, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { reduxStore } from '../redux/store';
 import { ReactNode } from 'react';
-import { hpApi } from '../redux/hpApi';
+import { useGetCharacterQuery, useSearchByNameQuery } from '../redux/hpApi';
+import { searchResponseMock } from './mocks/responses/searchResponseMock';
+import { clearSearchResponseMock } from './mocks/responses/clearSearchResponseMock';
+import { characterResponseMock } from './mocks/responses/characterResponseMock';
 
 function Wrapper(props: { children: ReactNode }) {
   return <Provider store={reduxStore}>{props.children}</Provider>;
 }
 
+const queryHarry = { searchValue: 'Harry', pageSize: 10, page: 1 };
+const clearQuery = { searchValue: '', pageSize: 10, page: 1 };
+const characterId = '309c1379-8579-4401-916e-bc6a17b2b39f';
+
 describe('toolkit query', () => {
-  it('renders hook', () => {
-    renderHook(() => hpApi.useSearchByNameQuery, { wrapper: Wrapper });
+  it('search string', async () => {
+    const { result } = renderHook(() => useSearchByNameQuery(queryHarry), {
+      wrapper: Wrapper,
+    });
+    await waitFor(() => {
+      expect(result.current.isSuccess).toBe(true);
+      expect(result.current.data).toMatchObject(searchResponseMock);
+    });
+  });
+  it('clear search', async () => {
+    const { result } = renderHook(() => useSearchByNameQuery(clearQuery), {
+      wrapper: Wrapper,
+    });
+    await waitFor(() => {
+      expect(result.current.isSuccess).toBe(true);
+      expect(result.current.data).toMatchObject(clearSearchResponseMock);
+    });
+  });
+  it('character', async () => {
+    const { result } = renderHook(() => useGetCharacterQuery(characterId), {
+      wrapper: Wrapper,
+    });
+    await waitFor(() => {
+      expect(result.current.isSuccess).toBe(true);
+      expect(result.current.data).toMatchObject(characterResponseMock);
+    });
   });
 });
--- test/mocks/handlers.ts
+++ test/mocks/handlers.ts
@@ -9,16 +9,14 @@ export const handlers = [
   http.get(`${baseUrl}characters/309c1379-8579-4401-916e-bc6a17b2b39f`, () => {
     return HttpResponse.json(characterResponseMock);
   }),
-  http.get(
-    `${baseUrl}characters/?filter[name_cont]=Harry&page[number]=1&page[size]=10`,
-    () => {
+  http.get(`${baseUrl}characters/`, ({ request }) => {
+    const url = new URL(request.url);
+    const search = url.searchParams.get('filter[name_cont]');
+    const page = url.searchParams.get('page[number]');
+    const pageSize = url.searchParams.get('page[size]');
+    if (search === 'Harry' && page === '1' && pageSize === '10')
       return HttpResponse.json(searchResponseMock);
-    }
-  ),
-  http.get(
-    `${baseUrl}characters/?filter[name_cont]=&page[number]=1&page[size]=10`,
-    () => {
+    if (search === '' && page === '1' && pageSize === '10')
       return HttpResponse.json(clearSearchResponseMock);
-    }
-  ),
+  }),
 ];
